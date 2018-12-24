d1d23c894b03521eb70755f4826f571b0fd3ef7f
kuangwenyi <kuangwinnie@gmail.com>
2018-12-24 22:06:02 +0800

    error jsondecoder

--- gTrans.py
+++ gTrans.py
@@ -1,4 +1,5 @@
-import googletrans
+from googletrans import Translator
+import multiprocessing as mp
 import os 
 import json
 
@@ -6,16 +7,25 @@ import json
 def main():
     folder = os.listdir('./')
     folder = [x for x in folder if x.endswith('.json')]
-    jsonPath = folder[0]
-    #for filepath in folder:
-    with open(jsonPathpy) as f:
-        print(json.load(f)['text'])
+    #jsonPath = folder[0]
+    tc = 0
+    for filepath in folder:
+        if tc < 5:
+            with open(filepath, 'r') as f:
+                rawText = json.load(f)['text']
+                print(trans(rawText))
+
+
 
 def trans(text):
     """
+    text: string
+    returnt: cnText, string
     text translate happens here.
     """
-    return
+    tL = Translator(timeout=12)
+    cnText = tL.translate(text, dest='zh-cn')
+    return cnText
 
 if __name__ == '__main__':
     main()
